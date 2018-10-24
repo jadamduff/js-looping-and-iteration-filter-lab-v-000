9246559edf3a0ee026a7aacb1b29da3634a3d1e4
Adam Duff <j.adam.duff@gmail.com>
2018-10-24 22:41:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file
 function findMatching(drivers, string) {
-  return drivers.filter(function (driver) {driver === string})
+  return drivers.filter(function (driver) {driver.toLowerCase() === string.toLowerCase()})
 }
