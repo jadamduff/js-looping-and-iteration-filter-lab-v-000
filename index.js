14e173610297033e727d328e641ff2f5944ede3d
Adam Duff <j.adam.duff@gmail.com>
2018-10-24 22:50:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ function findMatching(drivers, string) {
 }
 
 function fuzzyMatch(drivers, string) {
-  return drivers.filter(function(driver) {return driver.slice(0, drivers.length - 1).toLowerCase() === string.toLowerCase()})
+  return drivers.filter(function(driver) {return driver.slice(0, string.length - 1).toLowerCase() === string.toLowerCase()})
 }
