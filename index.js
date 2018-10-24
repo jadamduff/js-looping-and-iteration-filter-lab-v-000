c85fc000f10de664f5e985360aeec0aaad7054f3
Adam Duff <j.adam.duff@gmail.com>
2018-10-24 22:45:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Code your solution in this file
 function findMatching(drivers, string) {
-  return drivers.filter(function (driver) {return driver.toLowerCase() === string.toLowerCase()})
+  return drivers.filter(function(driver) {return driver.toLowerCase() === string.toLowerCase()})
+}
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter(function(driver) {return driver[0] === string})
 }
